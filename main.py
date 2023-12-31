517658a2de94e59f5e29c3d8adf69a9b043830f7
SokolovaKaterina <kat-1306@mail.ru>
2023-12-31 04:17:40 +0300

    updated print

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 class Cat:
-    # атрибуты класса - доступны всем
     est_hvost = True
 
-    # атрибуты экземпляра
-    # методы класса
-    # __init__ - метод, который выполняется автоматически при создании экземпляра класса
     def __init__(self, color, age):  # дантер методы
         self.color = color
         self.age = age
@@ -20,3 +16,6 @@ cat_petya = Cat("white", 2)
 
 Cat.meow(cat_vaska)
 cat_vaska.meow()
+
+
+print("Hello")
